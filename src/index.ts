0fbd2e7d93c4dc36b1d308daaef195e7982a38ae
Anamul Haque <anamulhaque9901@gmail.com>
2024-10-26 10:25:57 +0600

    create and get data

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+    // create a data
+    // const result = await prisma.post.create({
+    //     data: {
+    //         title: "This is title 2!",
+    //         content: "This is content...2",
+    //         authorName: "Anamul Haque"
+    //     }
+    // })
+
+    // console.log(result);
+
+    const getAllFromDB = await prisma.post.findMany();
+    console.log(getAllFromDB);
+}
+ 
+main();
